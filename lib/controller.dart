d0b84d25c0b185066340ed6a65cee621f3ace8f2
hongzhiqing <hzqbetterman@gmail.com>
2020-03-19 12:16:32 +0800

    添加监听事件

--- lib/controller.dart
+++ lib/controller.dart
@@ -2,6 +2,14 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
+typedef FirstRenderedStartListener = Function();
+
+///当前播放进度更新
+typedef OnPositionUpdateListener = Function(int position);
+
+///当前缓存进度更新
+typedef OnBufferedPositionUpdateListener = Function(int position);
+
 enum AVPEventType {
   ///准备完成事件*/
   AVPEventPrepareDone,
@@ -27,17 +35,62 @@ enum AVPEventType {
   ///循环播放开始事件*/
   AVPEventLoopingStart,
 }
-typedef FirstRenderedStartListener = Function();
+
+class AVPCacheConfig {
+  ///缓存目录
+  final String path;
+
+  ///单位秒
+  final int maxDuration;
+
+  ///单位M
+  final int maxSizeMB;
+
+  AVPCacheConfig({this.path, this.maxDuration, this.maxSizeMB});
+
+  Map<String, dynamic> toJson() =>
+      {"path": path, "maxDuration": maxDuration, "maxSizeMB": maxSizeMB};
+}
+
 class FAliListPlayerController {
   MethodChannel _channel;
   StreamSubscription _streamSubscription;
-  List<String> urls;
-  bool isAutoPlay;
-  bool firstRenderedStart = false;
-  FirstRenderedStartListener firstRenderedStartListener;
 
-  FAliListPlayerController({this.isAutoPlay = false}) {
+  ///播放列表的URL组
+  List<String> urls;
+
+  ///自动播放
+  bool isAutoPlay;
+
+  ///标记第一帧渲染成功,每次都切换新的视频都会标记为true
+  ///用到的地方需要手动标记为false，否则会一直为true
+  bool firstRenderedStart = false;
+
+  ///缓存配置
+  AVPCacheConfig cacheConfig;
+
+  ///第一帧渲染成功的监听器，每次切换新的视频都会调用
+  FirstRenderedStartListener _firstRenderedStartListener;
+
+  OnBufferedPositionUpdateListener _bufferedPositionUpdateListener;
+
+  OnPositionUpdateListener _positionUpdateListener;
+
+  set setBufferedPositionUpdateListener(
+      OnBufferedPositionUpdateListener value) {
+    _bufferedPositionUpdateListener = value;
+  }
+
+  set setPositionUpdateListener(OnPositionUpdateListener value) {
+    _positionUpdateListener = value;
+  }
+
+  FAliListPlayerController({this.isAutoPlay = false, this.cacheConfig}) {
     urls = List();
+  }
+
+  void setCacheConfig(AVPCacheConfig config) {
+    this.cacheConfig = config;
   }
 
   void addUrls(List<String> urls) {
@@ -46,22 +99,22 @@ class FAliListPlayerController {
 
   onViewCreate(int i) {
     if (_channel == null && _streamSubscription == null) {
-      _channel = MethodChannel("plugin.iqingbai.com/ali_video_play_0");
+      _channel = MethodChannel("plugin.iqingbai.com/ali_video_play_$i");
 
       _streamSubscription =
-          EventChannel("plugin.iqingbai.com/eventChannel/ali_video_play_0")
+          EventChannel("plugin.iqingbai.com/eventChannel/ali_video_play_$i")
               .receiveBroadcastStream()
               .listen(_onEvent);
 
       _channel.invokeMethod("addUrlSource", <String, dynamic>{"urls": urls});
       if (isAutoPlay || urls.length > 0) {
-        this.moveTo(urls[0]);
+        this.moveTo(0);
       }
     }
   }
 
-  setFirstRenderedStartListener(FirstRenderedStartListener listener){
-    this.firstRenderedStartListener = listener;
+  setFirstRenderedStartListener(FirstRenderedStartListener listener) {
+    this._firstRenderedStartListener = listener;
   }
 
   ///开始播放
@@ -85,11 +138,17 @@ class FAliListPlayerController {
   }
 
   ///移动到目标视频，并播放
-  ///[url]是视频的标识
-  Future<void> moveTo(String url) {
+  ///[index]是视频的标识坐标
+  Future<void> moveTo(int index) {
     return _channel?.invokeMethod("moveTo", {
-      "url": url,
+      "url": urls[index],
     });
+  }
+
+  ///获取缓存文件的路径
+  ///[url]文件的url
+  Future<void> getCachePath(String url) {
+    return _channel?.invokeMethod("getCachesPath");
   }
 
   void _onEvent(event) {
@@ -105,7 +164,17 @@ class FAliListPlayerController {
         print('onPlayerStatusChanged:${event["values"]}');
         if (event["values"] == 3) {
           firstRenderedStart = true;
-          this.firstRenderedStartListener();
+          this._firstRenderedStartListener();
+        }
+        break;
+      case "onCurrentPositionUpdate":
+        if (this._positionUpdateListener != null) {
+          this._positionUpdateListener(event["values"]);
+        }
+        break;
+      case "onBufferedPositionUpdate":
+        if (this._bufferedPositionUpdateListener != null) {
+          this._bufferedPositionUpdateListener(event["values"]);
         }
         break;
     }
--- lib/faliplayer.dart
+++ lib/faliplayer.dart
@@ -1,20 +1,17 @@
-import 'dart:async';
-import 'dart:math' as math;
 import 'package:faliplayer/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class AliPlayerView extends StatefulWidget {
-  final List<String> urls;
+  final FAliListPlayerController controller;
 
-  const AliPlayerView({Key key, this.urls}) : super(key: key);
+  const AliPlayerView({Key key, this.controller}) : super(key: key);
 
   @override
   _AliPlayerViewState createState() => _AliPlayerViewState();
 }
 
 class _AliPlayerViewState extends State<AliPlayerView> {
-  FAliListPlayerController _controller;
   PageController _pageController = PageController();
   int currentIndex = 0;
 
@@ -22,13 +19,11 @@ class _AliPlayerViewState extends State<AliPlayerView> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    _controller = FAliListPlayerController(isAutoPlay: true);
-    _controller.addUrls(widget.urls);
-    _controller.setFirstRenderedStartListener(() {
-      print('firstRenderedStart:${_controller.firstRenderedStart}');
-      if (_controller.firstRenderedStart) {
+    widget.controller.setFirstRenderedStartListener(() {
+      print('firstRenderedStart:${widget.controller.firstRenderedStart}');
+      if (widget.controller.firstRenderedStart) {
         setState(() {});
-        _controller.firstRenderedStart = false;
+        widget.controller.firstRenderedStart = false;
       }
     });
     _pageController.addListener(() {
@@ -36,7 +31,7 @@ class _AliPlayerViewState extends State<AliPlayerView> {
       if (isIntegerForDouble(_pageController.page)) {
         double page = _pageController.page;
         if (page != currentIndex) {
-          _controller.moveTo(widget.urls[page.toInt()]);
+          widget.controller.moveTo(page.toInt());
           currentIndex = page.toInt();
         }
       }
@@ -53,7 +48,9 @@ class _AliPlayerViewState extends State<AliPlayerView> {
           color: Colors.black,
         );
         if (currentIndex == i) {
-          body = VideoView(controller: _controller,);
+          body = VideoView(
+            controller: widget.controller,
+          );
         }
         return Container(
           color: Colors.black,
@@ -62,18 +59,19 @@ class _AliPlayerViewState extends State<AliPlayerView> {
             children: <Widget>[
               Container(
                 color: Colors.black,
-                child: Image.network("${widget.urls[i]}?vframe/jpg/offset/1"),
+                child: Image.network(
+                    "${widget.controller.urls[i]}?vframe/jpg/offset/1"),
               ),
               Offstage(offstage: !(currentIndex == i), child: body),
             ],
           ),
         );
       },
-      itemCount: _controller.urls.length,
+      itemCount: widget.controller.urls.length,
       onPageChanged: (i) {
         print('i=$i === current=$currentIndex');
         if (i >= currentIndex + 2 || i <= currentIndex - 2) {
-//          _controller.moveTo(widget.urls[i]);
+//          widget.controller.moveTo(widget.urls[i]);
 //          Future.delayed(Duration(milliseconds: 150)).then((d) {
 //            currentIndex = i;
 //          });
@@ -83,7 +81,7 @@ class _AliPlayerViewState extends State<AliPlayerView> {
   }
 
   bool isIntegerForDouble(double obj) {
-    if(obj==0){
+    if (obj == 0) {
       return true;
     }
     double eps = 1e-10; // 精度范围
@@ -106,6 +104,9 @@ class _VideoViewState extends State<VideoView> {
   Widget build(BuildContext context) {
     return UiKitView(
       viewType: "plugin.iqingbai.com/ali_video_play",
+      creationParams: {
+        "cacheConfig": widget.controller.cacheConfig?.toJson(),
+      },
       creationParamsCodec: const StandardMessageCodec(),
       onPlatformViewCreated: widget.controller.onViewCreate,
     );
